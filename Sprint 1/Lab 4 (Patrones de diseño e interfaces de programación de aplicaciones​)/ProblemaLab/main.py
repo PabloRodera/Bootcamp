4843bd9a2364950a654ebec1ff1d2cb683d4bba1
PabloRodera <pablo_rodera@hotmail.com>
2024-04-10 12:14:17 +0200

    Explicación de código y realización de video comprobando que la API funciona correctamente.

--- Sprint 1/Lab 4 (Patrones de diseño e interfaces de programación de aplicaciones​)/ProblemaLab/main.py
+++ Sprint 1/Lab 4 (Patrones de diseño e interfaces de programación de aplicaciones​)/ProblemaLab/main.py
@@ -28,3 +28,13 @@ def division(num1: int, num2: int):
     if num2 == 0:
         raise HTTPException(status_code=400, detail="No se puede dividir entre cero")
     return{"Resultado de la división" : num1 / num2}
+
+# Explicación breve del codigo:
+
+# Se importa la clase FastAPI del módulo fastapi.
+# Se crea una instancia de FastAPI llamada app.
+# Se crea un Endpoint raíz para dar la bienvenida cuando se accede a la FastAPI.
+# Se crean los Endpoints correspondientes para cada operación aritmética con sus dos respectivos parámetros de entrada.
+# Se realiza la operación correspondiente (+, -, *, /) y se devuelve el resultado en un diccionario JSON.
+# Se valida el caso en que se intente dividir por cero en el endpoint de división.
+# Se utiliza HTTPException para devolver un código de estado 400 (Bad Request) con un mensaje de error explicativo.
